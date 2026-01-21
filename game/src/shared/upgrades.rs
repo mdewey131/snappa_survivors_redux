0fdd53d06820a0e6ffe046abc32fb41380ac7aac
michael <michael.dewey131@gmail.com>
2026-01-21 14:29:36 -0800

    Adds modifications to file before branch

--- game/src/shared/upgrades.rs
+++ game/src/shared/upgrades.rs
@@ -1,18 +1,28 @@
 use crate::shared::weapons::WeaponKind;
 use bevy::prelude::*;
 
+/// The component that marks a given upgrade.
+/// Players will be offered one of three choices for them
+/// to take, which will boost their stats depending on
+/// the upgrade kind (which provides base values),
+/// and the rarity (which modifies those values)
 #[derive(Component)]
 pub struct Upgrade {
     pub kind: UpgradeKind,
+    pub rarity: UpgradeRarity,
+    pub level: u8,
+}
+
+pub enum UpgradeRarity {
+    Common,
+    Rare,
+    Epic,
+    Legendary,
 }
 
 pub enum UpgradeKind {
     UpgradeWeapon(WeaponKind),
-    UpgradeStat(StatUpgrade),
-}
-pub struct StatUpgrade {
-    pub sk: StatUpgradeKind,
-    pub v: f32,
+    UpgradeStat(StatUpgradeKind),
 }
 
 pub enum StatUpgradeKind {
