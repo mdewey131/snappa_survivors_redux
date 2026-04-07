51f19dcdb286f3673cf6e8f78f211c7368b03d5f
michael <michael.dewey131@gmail.com>
2026-04-06 18:15:06 -0700

    Further refinements to this system

--- game/src/shared/loading.rs
+++ game/src/shared/loading.rs
@@ -1,35 +1,74 @@
 use crate::shared::{
     combat::CharacterFacing, game_kinds::*, game_object_spawning::*, lobby::PlayerInLobby,
-    players::*, upgrades::PlayerUpgradeSlots, weapons::*,
+    players::*, states::AppState, upgrades::PlayerUpgradeSlots, weapons::*,
 };
 use avian2d::prelude::*;
-use bevy::{asset::LoadState, prelude::*};
+use bevy::prelude::*;
 use lightyear::prelude::*;
 use rand::Rng;
+
+const EXPECTED_LOADING_CONFIRMATION_FRAMES: u8 = 30;
+
+/// The set of common functions that are needed by both server and client.
+///
+/// This is primarily responsible to keep track of what's available in `LoadingAssets`
+pub struct SharedLoadingPlugin;
+impl Plugin for SharedLoadingPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_state::<LevelLoadingState>()
+            .insert_resource(LoadingAssets::new(EXPECTED_LOADING_CONFIRMATION_FRAMES))
+            .add_systems(
+                Update,
+                check_loading_assets.run_if(in_state(AppState::LoadingLevel)),
+            );
+    }
+}
 
 #[derive(Resource, Debug)]
 pub struct LoadingAssets {
     pub handles: Vec<UntypedHandle>,
+    pub max_confirmation_frames: u8,
+    pub curr_confirmation_frames: u8,
 }
 
-pub fn check_loading_assets(mut loading: ResMut<LoadingAssets>, assets: Res<AssetServer>) {
-    let _span = trace_span!("Checking Loading Status").entered();
-    let mut to_rm = Vec::new();
-    for (i, handle) in loading.handles.iter().enumerate() {
-        let load_state = assets
-            .get_load_state(handle)
-            .expect("Looking for a handle that doesn't exist!");
+#[derive(States, Default, Clone, Copy, Hash, Eq, PartialEq, Debug)]
+pub enum LevelLoadingState {
+    #[default]
+    LevelReady,
+    LevelLoading,
+}
 
-        match load_state {
-            LoadState::Loaded => to_rm.push(i),
-            LoadState::Loading => {}
-            LoadState::NotLoaded => {}
-            LoadState::Failed(_error) => {}
+impl LoadingAssets {
+    pub fn new(max_frames: u8) -> Self {
+        Self {
+            handles: Vec::new(),
+            max_confirmation_frames: max_frames,
+            curr_confirmation_frames: 0,
         }
     }
+}
 
-    for i in to_rm.iter().rev() {
-        loading.handles.remove(*i);
+pub fn check_loading_assets(
+    mut load_state: ResMut<NextState<LevelLoadingState>>,
+    mut loading: ResMut<LoadingAssets>,
+    assets: Res<AssetServer>,
+) {
+    let _span = trace_span!("Checking Loading Status").entered();
+    if !loading.handles.is_empty() {
+        // Set confirmation frames to 0 just in case
+        loading.curr_confirmation_frames = 0;
+
+        loading.handles.retain(|handle| {
+            // You'll thank me later for doing this recursively
+            assets
+                .get_recursive_dependency_load_state(handle)
+                .is_none_or(|state| !state.is_loaded())
+        });
+    } else {
+        loading.curr_confirmation_frames += 1;
+        if loading.curr_confirmation_frames == loading.max_confirmation_frames {
+            load_state.set(LevelLoadingState::LevelReady)
+        }
     }
 }
 
